ef49d1c6bd230155b6e9dffb649aace18123a8f1
Srivasthava Nunna <snunna@berkadia.com>
2018-11-20 19:45:50 +0530

    Changed the Formate of the mail

--- config/formatesformail.js
+++ config/formatesformail.js
@@ -1,7 +1,7 @@
 module.exports.formateForMail = function (type, textInput) {
     switch (type) {
         case 'forgotPassword':
-            const msg = '<p style="font-size:15px;">We received a request to change the password associated with this e-mail address</p><p style="font-size:25px;">Please click  the link that is given below</p><a href = "http://localhost:3000/resetpassword?index=' + textInput + '">Click me!!</a><p>Thank You</p>';
+            const msg = '<p style="font-size:15px;">We received a request to change the password associated with this e-mail address</p><p style="font-size:25px;">Please click  the link that is given below</p><a href = "https://auth-with-react.herokuapp.com/resetpassword?index=' + textInput + '">Click me!!</a><p>Thank You</p>';
             return msg
         default:
             return 'HELLO';
